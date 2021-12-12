9654543f5613ed5b75f1c1e774febc76990837ac
Celine Lefarth <celinelefarth@t-online.de>
2021-12-12 13:59:28 +0100

    Aufgabe09

---
+++ Übung/script.ts
@@ -0,0 +1,32 @@
+/*Bilden Sie drei Personen dieses Kurses durch Objekte ab. 
+Erstellen Sie dazu ein Interface, dass alle relevanten Eigenschaften dieser 
+Personen beinhaltet.*/
+
+interface MKB1 {
+    Vorname : string;
+    Nachname : string;
+    Alter : number;
+    Job : string;
+}
+
+var Tim : MKB1 = {
+    Vorname: "Tim",
+    Nachname: "Schulz",
+    Alter: 43,
+    Job: "Verkäufer"
+}
+
+var Sophie : MKB1 = {
+    Vorname: "Sophie",
+    Nachname: "Schmidt",
+    Alter: 23,
+    Job: "Student"
+}
+
+var Celine : MKB1 = {
+    Vorname: "Celine",
+    Nachname: "Lefarth",
+    Alter: 20,
+    Job: "Mediengestalter"
+}
+
